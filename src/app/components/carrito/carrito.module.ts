60fc566a4ed7928c9b229aba8465ccf27b25e5b4
JuliannaFaican <julifc2005@gmail.com>
2023-01-07 19:23:47 -0500

    creaciondecomponentes

---
+++ src/app/components/carrito/carrito.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CarritoComponent } from './carrito.component';
+
+
+
+@NgModule({
+  declarations: [CarritoComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class CarritoModule { }
---
+++ src/app/components/factura/factura.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FacturaComponent } from './factura.component';
+
+
+
+@NgModule({
+  declarations: [FacturaComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class FacturaModule { }
